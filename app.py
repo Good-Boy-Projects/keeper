053cf69a612cf95912692dc0b7d607b9007cdcc3
matt <studio@goodboyprojects.net>
2026-05-23 16:17:22 -0400

    update app.py for reading path

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from db import get_bookmarks, get_all_folders, get_all_tags
+from db import get_bookmarks, get_bookmark, get_all_folders, get_all_tags
 
 app = Flask(__name__)
 
@@ -10,6 +10,19 @@ def index():
     tags = get_all_tags()
     return render_template("index.html",
         bookmarks=bookmarks,
+        folders=folders,
+        tags=tags
+    )
+
+@app.route("/bookmark/<int:id>")
+def reading(id):
+    bookmark = get_bookmark(id)
+    if not bookmark:
+        return "Bookmark not found", 404
+    folders = get_all_folders()
+    tags = get_all_tags()
+    return render_template("reading.html",
+        bookmark=bookmark,
         folders=folders,
         tags=tags
     )
